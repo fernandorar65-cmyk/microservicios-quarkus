a9414eaca2b48a9e046e573fb3ed7840ecdb91cd
FernandoRar <fernandorar65@gmail.com>
2026-08-12 10:24:22 -0500

    add: configuration basic

--- infrastructure/postgres/init-databases.sql
+++ infrastructure/postgres/init-databases.sql
@@ -1,3 +1,3 @@
 -- Temporary local setup: one shared Postgres database for all microservices.
 -- Later each service will use its own DB again (identity_db, organization_db, ...).
-CREATE DATABASE kahoot_db;
+CREATE DATABASE clabs;
