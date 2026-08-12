a78990240df721b6e64c72f465a509761d03edfc
FernandoRar <fernandorar65@gmail.com>
2026-08-12 11:31:34 -0500

    adjasdn

--- infrastructure/postgres/init-databases.sql
+++ infrastructure/postgres/init-databases.sql
@@ -1,3 +1,1 @@
--- Temporary local setup: one shared Postgres database for all microservices.
--- Later each service will use its own DB again (identity_db, organization_db, ...).
 CREATE DATABASE clabs;
